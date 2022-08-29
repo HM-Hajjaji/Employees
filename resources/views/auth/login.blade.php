a6de272ff96777bd0ea09e0527ba89476b4ed0df
HHajjaji.FSD <HHajjaji.FSD@gmail.com>
2022-08-29 17:00:01 +0100

    change nav dashboard and remove page welcom and edit page login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,8 @@
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+{{--                <x-application-logo  />--}}
+                <img src="{{asset('images/user-profile.png')}}" alt="login" class="w-20 h-20 fill-current text-gray-500">
             </a>
         </x-slot>
 
@@ -42,8 +43,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                        {{ __('I do not have an account') }}
                     </a>
                 @endif
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,11 +12,36 @@
         </li>
     </ul>
     <ul class="navbar-nav ml-auto">
-
         <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-                <i class="fas fa-th-large"></i>
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
+        @if(Route::has("login"))
+            @auth
+                <li class="nav-item mr-3">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{Auth::user()->name}}
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark">
+                            <form action="{{route('logout')}}" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item active">Logout</button>
+                            </form>
+                        </ul>
+                    </div>
+                </li>
+            @else
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="{{route('login')}}" class="nav-link">Log in</a>
+                </li>
+                @if(Route::has('register'))
+                    <li class="nav-item d-none d-sm-inline-block">
+                        <a href="{{route('register')}}" class="nav-link">Register</a>
+                    </li>
+                @endif
+            @endauth
+        @endif
     </ul>
 </nav>
